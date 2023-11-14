d7830726a94ebb1cebe2f0dadb30266792dd02ab
Caldoon222 <150370026+Caldoon222@users.noreply.github.com>
2023-11-14 14:56:47 +0300

    Update CoinDetectionFunc.cpp
    Update for ChooseColor func

--- CoinDetectionFunc.cpp
+++ CoinDetectionFunc.cpp
@@ -1,24 +1,33 @@
 #include"Header.h"
+#include"Color.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 
 
 
+int b, g, r;
+
+
 
 void CoinDetectionWebCam()
 {
+	int b, g, r;
 		cv::VideoCapture cap(0);
+		cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+		cap.set(cv::CAP_PROP_FRAME_HEIGHT, 340);
+
 
 		if (!cap.isOpened())  // if no webcam exit program
 		{
 			std::cout << "\n\n\n\n===============================\nError: Cannot open the webcam\n===============================\n\n\n\n";
 			system("pause");
+			exit(0);
 		}
 
-		cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-		cap.set(cv::CAP_PROP_FRAME_HEIGHT, 340);
 
+
+		ChooseColor(b, g, r);
 
 
 
@@ -45,11 +54,11 @@ void CoinDetectionWebCam()
 			if (!circles.empty()) {
 				for (int i = 0; i < circles.size(); i++) {
 					cv::Vec3i c = circles[i];
-					cv::Point center = cv::Point(c[0], c[1]);	// circle center
-					circle(image, center, 1, cv::Scalar(255, 255, 0), 3, cv::LINE_AA);	// circle outline
+					cv::Point center = cv::Point(c[0], c[1]);	
+					circle(image, center, 1, cv::Scalar(b, g, r), 3, cv::LINE_AA); // circle center
 
 					int radius = c[2];
-					circle(image, center, radius, cv::Scalar(255, 0, 255), 3, cv::LINE_AA);
+					circle(image, center, radius, cv::Scalar(b, g, r), 3, cv::LINE_AA);	// circle outline
 					if (radius > 1)
 						TotalMoney += 1;
 					//std::cout << radius << '\n';
@@ -60,8 +69,7 @@ void CoinDetectionWebCam()
 			putText(image, "Total Money: " + std::to_string(TotalMoney), cv::Point(1, 100), cv::FONT_HERSHEY_PLAIN, 2, 10, 4);
 			imshow("circles", image);
 			
-			cv::waitKey(1);	
-			
+			cv::waitKey(30);
 		}
 }
 
@@ -77,10 +85,11 @@ void CoinDetectionImage()
 	std::cout << "Enter file path(whith file name and format(.jpg .png): ";
 	std::cin >> path;
 
-
-
-
 	cv::Mat image = cv::imread(path);
+
+
+
+
 
 	if (image.empty())  // if no image exit program
 	{
@@ -103,39 +112,40 @@ void CoinDetectionImage()
 	std::cin >> coinsdist;
 
 
+
+	ChooseColor(b, g, r);
+
+
 	while (true) {
 		int TotalMoney = 0;	//reseting 
 
 		cv::Mat image = cv::imread(path);
 		//load image
 
-
-
-
 		cv::Mat gray;
 		cvtColor(image, gray, cv::COLOR_BGR2GRAY);
 		cv::medianBlur(gray, gray, 5);
 
 
-
-
 		std::vector<cv::Vec3f> circles;
-		cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows / coinsdist,100, 30, 10, 200);
-						//coinsdist - detect circles with different distances to each other		// 2 last par min_radius & max_radius 
+		HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows / 8, 100, 30, 10, 200);
+		// 2 last par min_radius & max_radius 
 
 		if (!circles.empty()) {
 			for (int i = 0; i < circles.size(); i++) {
 				cv::Vec3i c = circles[i];
-				cv::Point center = cv::Point(c[0], c[1]);	// circle center
-				circle(image, center, 1, cv::Scalar(255, 255, 0), 3, cv::LINE_AA);	// circle outline
+				cv::Point center = cv::Point(c[0], c[1]);
+				circle(image, center, 1, cv::Scalar(b, g, r), 3, cv::LINE_AA); // circle center
 
 				int radius = c[2];
-				circle(image, center, radius, cv::Scalar(255, 0, 255), 3, cv::LINE_AA);
+				circle(image, center, radius, cv::Scalar(b, g, r), 3, cv::LINE_AA);	// circle outline
 				if (radius > 1)
 					TotalMoney += 1;
 				//std::cout << radius << '\n';
 			}
 		}
+		
+
 		
 	
 	
@@ -145,6 +155,5 @@ void CoinDetectionImage()
 
 		
 		cv::waitKey(0);	
-		system("pause");
 	}
 }
