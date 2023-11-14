cb35a5d8cb1ee472dfae54b36b894a317dffe072
Caldoon222 <150370026+Caldoon222@users.noreply.github.com>
2023-11-14 15:00:01 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
 #include "Header.h"
-
-
-
-
-
 
 
 int main()
@@ -12,15 +6,18 @@ int main()
 	int check;
 	std::cout << "Are you using Webcam(1) or Image(0) Enter number: ";
 	std::cin >> check;
+
+
+
 	std::cout << '\n';
 	switch (check)
 	{
 	 case(1):
 		CoinDetectionWebCam();
+		break;
 	 case(0):
 		CoinDetectionImage();
+		break;
 	}
-
-
 	return 0;
 }
